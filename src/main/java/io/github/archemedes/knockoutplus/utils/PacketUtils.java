7138ae26383c7ae973db25aa2281fd91a399f3f9
Matthew Hildebrand <korvic@live.ca>
2019-12-15 01:36:16 -0600

    Updated index to 13 instead of 12 for sleeping position.

--- src/main/java/io/github/archemedes/knockoutplus/utils/PacketUtils.java
+++ src/main/java/io/github/archemedes/knockoutplus/utils/PacketUtils.java
@@ -22,7 +22,7 @@ public class PacketUtils {
 		watcher.setEntity(player);
 		// TODO: Remove NMS when protocolib updates
 		watcher.setObject(6, WrappedDataWatcher.Registry.get(EntityPose.class), EntityPose.SLEEPING);
-		watcher.setObject(12, WrappedDataWatcher.Registry.get(BlockPosition.class, true), Optional.of(new BlockPosition(location.getBlockX(), location.getBlockY() - 3, location.getBlockZ())));
+		watcher.setObject(13, WrappedDataWatcher.Registry.get(BlockPosition.class, true), Optional.of(new BlockPosition(location.getBlockX(), location.getBlockY() - 3, location.getBlockZ())));
 
 		sleepPacket.getWatchableCollectionModifier().write(0, watcher.getWatchableObjects());
 
@@ -36,7 +36,7 @@ public class PacketUtils {
 		WrappedDataWatcher watcher = new WrappedDataWatcher();
 		watcher.setEntity(player);
 		watcher.setObject(6, WrappedDataWatcher.Registry.get(EntityPose.class), EntityPose.STANDING);
-		watcher.setObject(12, WrappedDataWatcher.Registry.get(BlockPosition.class, true), Optional.empty());
+		watcher.setObject(13, WrappedDataWatcher.Registry.get(BlockPosition.class, true), Optional.empty());
 
 		sleepPacket.getWatchableCollectionModifier().write(0, watcher.getWatchableObjects());
 
